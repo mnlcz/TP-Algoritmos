ad76e8928193502f198fde081f5291fb1dca4ebd
mnlcz <manuel.lopez.cosmitz@gmail.com>
2022-04-13 21:11:20 -0300

    Se agregó un método que valide el movimiento.

--- EnumDireccion.cs
+++ EnumDireccion.cs
@@ -26,8 +26,8 @@
                 "D" => Direccion.DERECHA,
                 "E" => Direccion.ARRIBA_DERECHA,
                 "Q" => Direccion.ARRIBA_IZQUIERDA,
-                "Z" => Direccion.ABAJO_DERECHA,
-                "C" => Direccion.ABAJO_IZQUIERDA,
+                "C" => Direccion.ABAJO_DERECHA,
+                "Z" => Direccion.ABAJO_IZQUIERDA,
                 _ => Direccion.INVALIDA
             };
             return d;
@@ -35,13 +35,54 @@
 
         public static void MostrarOpciones()
         {
-            var keys = new[] { 'W', 'S', 'A', 'D', 'E', 'Q', 'Z', 'C' };
+            var keys = new[] { 'W', 'S', 'A', 'D', 'E', 'Q', 'C', 'Z' };
             var query = from d
                        in Enum.GetValues(typeof(Direccion)).Cast<Direccion>()
                         where d != Direccion.INVALIDA
                         select d;
             for(int i = 0; i < keys.Length; i++)
                 Console.WriteLine($"{query.ElementAt(i)} ==> {keys[i]}");
+        }
+
+        public static bool MovimientoValido(this Direccion d, Coordenada c)
+        {
+            switch(d)
+            {
+                case Direccion.DERECHA:
+                    c = new Coordenada(c.X, c.Y + 1);
+                    if (c.Y > 9) return false;
+                    break;
+                case Direccion.IZQUIERDA:
+                    c = new Coordenada(c.X, c.Y - 1);
+                    if (c.Y < 0) return false;
+                    break;
+                case Direccion.ABAJO:
+                    c = new Coordenada(c.X + 1, c.Y);
+                    if(c.X > 9) return false;
+                    break;
+                case Direccion.ARRIBA:
+                    c = new Coordenada(c.X - 1, c.Y);
+                    if(c.X < 0) return false;
+                    break;
+                case Direccion.ABAJO_DERECHA:
+                    c = new Coordenada(c.X + 1, c.Y + 1);
+                    if (c.Y > 9 || c.X > 9) return false;
+                    break;
+                case Direccion.ARRIBA_DERECHA:
+                    c = new Coordenada(c.X - 1, c.Y + 1);
+                    if(c.X < 0 || c.Y > 9) return false;
+                    break;
+                case Direccion.ABAJO_IZQUIERDA:
+                    c = new Coordenada(c.X + 1, c.Y - 1);
+                    if (c.Y < 0 || c.X > 9) return false;
+                    break;
+                case Direccion.ARRIBA_IZQUIERDA:
+                    c = new Coordenada(c.X - 1, c.Y - 1);
+                    if (c.Y < 0 || c.X < 0) return false;
+                    break;
+                default: return false;
+            }
+            return true;
         }
     }
 }
